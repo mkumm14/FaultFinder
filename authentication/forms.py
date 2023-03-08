8b5d8417501472da2cf3608ec95c4dfd32135ca0
mkumm14 <madhavkummara@outlook.com>
2023-03-07 18:03:30 -0600

    added username field to both form and user-info

--- authentication/forms.py
+++ authentication/forms.py
@@ -106,11 +106,12 @@ class RegisterForm(UserCreationForm):
 
 class editUserForm(forms.ModelForm):
       
+    username=forms.CharField(help_text=None)
     first_name=forms.CharField(required=True)
     last_name=forms.CharField(required=True)
     email=forms.EmailField(required=True)
     class Meta:
         model=User
-        fields=['first_name','last_name','email']
+        fields=['username','first_name','last_name','email']
     
 
