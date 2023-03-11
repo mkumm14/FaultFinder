9e47fd82ca414655721d9fc1d4879a6be955d668
mkumm14 <madhavkummara@outlook.com>
2023-03-11 01:21:27 -0600

    add project view functionality

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
-    path('add-project/',views.add_project, name='add-project')
+    path('add-project/',views.add_project, name='add-project'),
+    path('<int:pk>/view/',views.project_view,name='project-view')
 ]
--- project/views.py
+++ project/views.py
@@ -3,6 +3,11 @@ from .forms import addProjectForm
 from .models import Project
 # Create your views here.
 
+
+def project_view(request,pk):
+    project= Project.objects.get(id=pk)
+    return render(request, 'project/project-view.html',{'project': project})
+
 def add_project(request):
     form=addProjectForm()
 
