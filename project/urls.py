8386aad20ba36b4dcaf9eab509663242932f3098
mkumm14 <madhavkummara@outlook.com>
2023-03-13 02:09:04 -0500

    add dashboard adn project specific urls

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
     path('add-project/',views.add_project, name='add-project'),
-    path('<int:pk>/view/',views.project_view,name='project-view')
+    path('<int:pk>/dashboard/',views.dashboard,name='dashboard')
 ]
--- project/views.py
+++ project/views.py
@@ -4,9 +4,9 @@ from .models import Project
 # Create your views here.
 
 
-def project_view(request,pk):
+def dashboard(request,pk):
     project= Project.objects.get(id=pk)
-    return render(request, 'project/project-view.html',{'project': project})
+    return render(request, 'project/dashboard.html',{'project': project})
 
 def add_project(request):
     form=addProjectForm()
