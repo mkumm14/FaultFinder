f325066307cad10a1bb15a6b558532fccdf42470
mkumm14 <madhavkummara@outlook.com>
2023-03-13 20:32:00 -0500

    add bugs table

--- project/views.py
+++ project/views.py
@@ -9,9 +9,9 @@ def dashboard_data(request, pk):
     project=Project.objects.get(id=pk)
     return render(request, 'project/partials/dashboard_data.html', {'project':project})
 
-def dashboard(request,pk):
+def project_view(request,pk):
     project= Project.objects.get(id=pk)
-    return render(request, 'project/dashboard.html',{'project': project})
+    return render(request, 'project/project-view.html',{'project': project})
 
 def add_project(request):
     form=addProjectForm()
