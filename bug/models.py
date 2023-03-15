8566919610cf5c8cdf0f8ae4066b52772c289551
mkumm14 <madhavkummara@outlook.com>
2023-03-15 02:01:55 -0500

    change assigned_to from ForeignKey to ManyToManyField

--- bug/models.py
+++ bug/models.py
@@ -11,7 +11,7 @@ class Bug(models.Model):
     status = models.CharField(max_length=20, choices=[('new', 'New'), ('in_progress', 'In Progress'), ('fixed', 'Fixed'), ('closed', 'Closed')], default='new')
     priority = models.CharField(max_length=20, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], default='medium')
     reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reported_bugs')
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_bugs', null=True, blank=True)
+    assigned_to = models.ManyToManyField(User, related_name='assigned_bugs', blank=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='bugs')
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
