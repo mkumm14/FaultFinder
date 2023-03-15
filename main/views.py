bcc11c8e75b66c4e13da35ab2cf6e2816fff4be4
mkumm14 <madhavkummara@outlook.com>
2023-03-15 02:10:58 -0500

    add bugs data to the home page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from bug.models import Bug
+
 # Create your views here.
 
 
 @login_required()
 def home(request):
-    return render(request, 'main/home.html')
+    user=request.user
+
+    num_open_bugs = Bug.objects.filter(assigned_to=user, status='new').count(
+    ) + Bug.objects.filter(assigned_to=user, status='in_progress').count()
+
+
+# Count the number of closed bugs
+    num_closed_bugs = Bug.objects.filter(assigned_to=user, status='fixed').count(
+    ) + Bug.objects.filter(assigned_to=user, status='closed').count()
+
+    return render(request, 'main/home.html', {'open_bugs':num_open_bugs, 'closed_bugs':num_closed_bugs})
