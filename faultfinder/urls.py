fc3d7523d4bc2ddd0b3f3c0881204c6981e130d2
mkumm14 <madhavkummara@outlook.com>
2023-09-13 18:25:57 -0500

    add redirect to main at index url

--- faultfinder/urls.py
+++ faultfinder/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth.views import LoginView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,7 +23,7 @@ from django.contrib.auth.forms import AuthenticationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name="index"),
+    path('', RedirectView.as_view(url='/main/'), name="index"),
     path('', include('authentication.urls')),
     path('project/', include('project.urls')),
     path('bug/', include('bug.urls')),
