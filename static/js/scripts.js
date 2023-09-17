9c36441a12e8d0d60e3641fc65f7d3cf2d762688
mkumm14 <madhavkummara@outlook.com>
2023-09-16 23:49:38 -0500

    fix sidebar toggle error and make some links hx-boost

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,24 @@
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-            document.body.classList.toggle('sb-sidenav-toggled');
-        }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
+//     // Toggle the side navigation
+//     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+//     if (sidebarToggle) {
+//         if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//         }
+//         sidebarToggle.addEventListener('click', event => {
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+//         });
+//     }
+
+// });
+document.addEventListener('click', function (event) {
+    if (event.target.closest('#sidebarToggle')) {
+        event.preventDefault();
+        document.body.classList.toggle('sb-sidenav-toggled');
+        localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
     }
 
 });
